bcf7c36e7e605b5267ea238d55967200e286be6a
c1gyoden <c1gyoden@vasvari.org>
2024-04-10 08:45:21 +0200

    class

---
+++ utonev.py
@@ -0,0 +1,24 @@
+class Utonev:
+    def __init__(self, nev, elso, masodik, ujsz1, ujsz2, nem):
+        self.nev = nev
+        self.elso = elso
+        self.masodik = masodik
+        self.ujsz1 = ujsz1
+        self.ujsz2 = ujsz2
+        self.nem = nem
+
+    def __str__(self):
+        return f'Név: {self.nev}\nElső: {self.elso}\nMásodik: {self.masodik}\nUjsz1: {self.ujsz1}\nUjsz2: {self.ujsz2}\nNem: {self.nem}\n'
+
+fajl = open('utonev.txt', 'rt', encoding='ansi')
+
+lista = []
+
+for sor in fajl:
+    sor = sor.strip().split(';')
+    lista.append(Utonev(sor[0], sor[1], sor[2], sor[3], sor[4], sor[5]))
+
+for a in lista:
+    print(a)
+
+    
